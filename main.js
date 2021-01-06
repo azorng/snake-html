29ae3ad071e781764f5478cf72e368420fa6c843
Azor <azorvk@gmail.com>
2021-01-06 09:23:33 +0100

    few improvements

--- main.js
+++ main.js
@@ -1,36 +1,34 @@
-/*
- * TODO:
- * Treat head as a body part
- *
- */
-
 const gameCtx = {
   direction: undefined,
-  speed: 150,
+  speed: 100,
   score: 0,
   moveHistory: []
-};
+}
 
-const screen = initScreen();
 const xCols = 20
-const pixelSize = parseInt(screen.style.width) / xCols;
-const head = initHead();
-screen.appendChild(head);
+const screenHeight = 500
+const screenWidth = 500
+const pixelSize = screenWidth / xCols
+
+const screen = initScreen()
+const menu = initMenu()
+const head = initHead()
+const body = []
+const food = []
 
 window.onload = () => {
-  document.getElementById('main').appendChild(screen);
-  initKeyBindings();
-  createFood(3)
-};
+  document.getElementById('main').appendChild(screen)
+  initKeyBindings()
+  spawnApple(3)
+}
 
 function start() {
   setInterval(() => {
-    moveNext();
-    handleScreenEdges();
-    handleCollision();
-    handleEat();
-    updateScore();
-  }, gameCtx.speed);
+    moveNext()
+    handleScreenEdges()
+    handleCollision()
+    handleEat()
+  }, gameCtx.speed)
 }
 
 function initKeyBindings() {
@@ -39,154 +37,159 @@ function initKeyBindings() {
     ArrowUp: 'Up',
     ArrowRight: 'Right',
     ArrowLeft: 'Left',
-  };
+  }
 
   document.onkeydown = function (e) {
-    const direction = keyMapping[e.key];
+    const direction = keyMapping[e.key]
     if (direction) {
-      if (!gameCtx.direction) start();
-      gameCtx.direction = direction;
+      if (!gameCtx.direction) start()
+      if (gameCtx.direction != oppositeDirection(direction))
+        gameCtx.direction = direction
     }
-  };
+  }
 }
 
 function handleScreenEdges() {
   handleEdge(head)
-  Array.from(document.getElementsByClassName('body')).forEach(bodyPart => {
+  body.forEach(bodyPart => {
     handleEdge(bodyPart)
   })
 }
 
 function handleEdge(el) {
   if (parseInt(el.style.top) > parseInt(screen.style.height) - pixelSize)
-    el.style.top = 0;
+    el.style.top = 0
 
   if (parseInt(el.style.top) < 0)
-    el.style.top = parseInt(screen.style.height) - pixelSize;
+    el.style.top = parseInt(screen.style.height) - pixelSize
 
   if (parseInt(el.style.left) > parseInt(screen.style.width) - pixelSize)
-    el.style.left = 0;
+    el.style.left = 0
 
   if (parseInt(el.style.left) < 0)
-    el.style.left = parseInt(screen.style.width) - pixelSize;
+    el.style.left = parseInt(screen.style.width) - pixelSize
 }
 
 function moveNext() {
   move(head, gameCtx.direction)
-  gameCtx.moveHistory.unshift(gameCtx.direction)
 
-  Array.from(document.getElementsByClassName('body')).forEach((part, i) => {
-    const direction = gameCtx.moveHistory[i+1]
+  body.forEach((part, i) => {
+    const direction = gameCtx.moveHistory[i]
     move(part, direction)
   })
+
+  gameCtx.moveHistory.unshift(gameCtx.direction)
 }
 
 function move(el, direction) {
   switch (direction) {
     case 'Down':
-      el.style.top = parseInt(el.style.top) + pixelSize;
-      break;
+      el.style.top = parseInt(el.style.top) + pixelSize
+      break
 
     case 'Up':
-      el.style.top = parseInt(el.style.top) - pixelSize;
-      break;
+      el.style.top = parseInt(el.style.top) - pixelSize
+      break
 
     case 'Right':
-      el.style.left = parseInt(el.style.left) + pixelSize;
-      break;
+      el.style.left = parseInt(el.style.left) + pixelSize
+      break
 
     case 'Left':
-      el.style.left = parseInt(el.style.left) - pixelSize;
-      break;
+      el.style.left = parseInt(el.style.left) - pixelSize
+      break
 
     default:
-      break;
+      break
   }
 }
 
 function initScreen() {
-  const screen = document.createElement('div');
-  screen.id = 'screen';
-  screen.style.width = 500;
-  screen.style.height = 500;
+  const screen = document.createElement('div')
+  screen.id = 'screen'
+  screen.style.width = screenWidth
+  screen.style.height = screenHeight
 
-  const menu = initMenu();
-  screen.appendChild(menu)
   return screen
 }
 
-function createFood(n = 1) {
+function spawnApple(n = 1) {
   const availablePositions = Array(xCols).fill().map((_, i) => pixelSize * i)
   for (let i = 0; i < n; i++) {
-    const food = document.createElement('div');
-    food.className = 'food';
-    food.style.height = pixelSize;
-    food.style.width = pixelSize;
-    food.style.left = availablePositions[rand(0, availablePositions.length)];
-    food.style.top = availablePositions[rand(0, availablePositions.length)];
-    screen.appendChild(food);
+    const apple = document.createElement('div')
+    apple.className = 'apple'
+    apple.style.height = pixelSize
+    apple.style.width = pixelSize
+    apple.style.left = availablePositions[rand(0, availablePositions.length)]
+    apple.style.top = availablePositions[rand(0, availablePositions.length)]
+    screen.appendChild(apple)
+    food.push(apple)
   }
 }
 
 function initMenu() {
-  const menu = document.createElement('div');
+  const menu = document.createElement('div')
   menu.id = 'menu'
-  menu.style.width = 500;
+  menu.style.width = 500
   menu.innerHTML = `Score: <span id='score'>${gameCtx.score}</span>`
-  
-  return menu;
+  screen.appendChild(menu)
+  return menu
 }
 
 function initHead() {
-  const head = document.createElement('div');
-  head.id = 'head';
+  const head = document.createElement('div')
+  head.id = 'head'
 
-  setBody(head);
-  return head;
+  initObj(head)
+  screen.appendChild(head)
+  return head
 }
 
 function rand(min, max) {
-  return parseInt(Math.random() * (max - min) + min);
+  return parseInt(Math.random() * (max - min) + min)
 }
 
 function handleEat() {
-  const food = Array.from(document.getElementsByClassName('food'));
   food.forEach(apple => {
-    if (apple.style.left == head.style.left && apple.style.top == head.style.top) {
-      gameCtx.score += 10
-      addBody();
-      apple.remove();
-      createFood();
+    if (isSamePos(head, apple)) {
+      addScore(10)
+      addBody()
+      removeApple(apple)
+      spawnApple()
     }
   })
 }
 
-function updateScore() {
-  const score = document.getElementById('score');
+function removeApple(apple) {
+  for (let i = 0; i <= food.length; i++) {
+    if (food[i] == apple) {
+      food.splice(i, 1)
+      apple.remove()
+      return
+    }
+  }
+}
+
+function addScore(points) {
+  gameCtx.score += points
+  const score = document.getElementById('score')
   score.innerHTML = gameCtx.score
 }
 
 function addBody() {
-  const body = document.createElement('div');
-  body.className = 'body';
-  setBody(body)
-  let direction
+  const bodyPart = document.createElement('div')
+  bodyPart.className = 'body-part'
+  initObj(bodyPart)
 
-  const bodyParts = Array.from(document.getElementsByClassName('body'))
+  const lastPart = body.length == 0 ? head : body[body.length - 1]
 
-  if (bodyParts.length == 0) {
-    body.style.left = head.style.left
-    body.style.top = head.style.top
-    direction = oppositeDirection(gameCtx.moveHistory[1])
-  } else {
-    const lastBody = bodyParts[bodyParts.length - 1]
-    body.style.left = lastBody.style.left
-    body.style.top = lastBody.style.top
-    direction = oppositeDirection(gameCtx.moveHistory[bodyParts.length])
-  }
+  bodyPart.style.left = lastPart.style.left
+  bodyPart.style.top = lastPart.style.top
+  direction = oppositeDirection(gameCtx.moveHistory[body.length])
 
-  move(body, direction)
-  screen.appendChild(body)
+  move(bodyPart, direction)
+  screen.appendChild(bodyPart)
+  body.push(bodyPart)
 }
 
 function oppositeDirection(direction) {
@@ -205,20 +208,23 @@ function oppositeDirection(direction) {
   }
 }
 
-function setBody(piece) {
-  piece.style.left = 0;
-  piece.style.right = 0;
-  piece.style.top = 0;
-  piece.style.bottom = 0;
-  piece.style.height = pixelSize;
-  piece.style.width = pixelSize;
+function initObj(piece) {
+  piece.style.left = 0
+  piece.style.right = 0
+  piece.style.top = 0
+  piece.style.bottom = 0
+  piece.style.height = pixelSize
+  piece.style.width = pixelSize
 }
 
 function handleCollision() {
-  Array.from(document.getElementsByClassName('body')).forEach(bodyPart => {
-    if (bodyPart.style.left == head.style.left && bodyPart.style.top == head.style.top) {
+body.forEach(bodyPart => {
+    if (isSamePos(head, bodyPart)) {
       alert("COLLISION!!")
     }
   })
+}
 
+function isSamePos(el1, el2) {
+    return el1.style.left == el2.style.left && el1.style.top == el2.style.top
 }
